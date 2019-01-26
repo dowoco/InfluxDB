c1e7e95a1b509b1da8c2dd0157c175b914542f5f
Dowoco <37535745+dowoco@users.noreply.github.com>
2019-01-26 15:13:23 +0000

    Update install_pi_collector.sh

--- install_pi_collector.sh
+++ install_pi_collector.sh
@@ -7,4 +7,14 @@ exec &> >(tee collector_install.log)
 exec 2>&1
 
 #Install the dependencies
-apt-get update
+sudo apt-get update
+
+#Install pip to allow Python stuff to be installed
+sudo apt-get install python-pip
+
+#Use pip to install psutils so that the collector can get CPU and memeory stats
+sudo pip install psutil
+
+#Install request to allow data to be sent
+sudo apt-get update
+sudo apt-get install python-requests
