0c1f09d29edddb9b107d9fe434fdf8ec7488805d
Dowoco <37535745+dowoco@users.noreply.github.com>
2019-01-26 23:05:55 +0000

    test.sh

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+# Create a log file of the build as well as displaying the build on the tty as it runs
+exec &> >(tee collector_install.log)
+exec 2>&1
+
+sudo apt-get update
+
+echo
+echo Finished!
